309b1f0a8e56ee456488269810c4d8424ef1046a
sivanesh-dsp <sivaneshsiva240@gmail.com>
2024-09-20 23:52:35 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "appId" {
+  description = "Azure Kubernetes Service Cluster service principal"
+}
+
+variable "password" {
+  description = "Azure Kubernetes Service Cluster password"
+}
+
+variable "subscription_id" {
+  description = "Subscription Id for azure"  
+}
