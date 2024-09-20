839a2cf071d3fad1dd7d93250c36fe3ba74934a7
sivanesh-dsp <sivaneshsiva240@gmail.com>
2024-09-20 23:52:27 +0530

    aks-cluster.tf

---
+++ aks-cluster.tf
@@ -0,0 +1,41 @@
+resource "random_pet" "prefix" {}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "default" {
+  name     = "${random_pet.prefix.id}-rg"
+  location = "East US"
+
+  tags = {
+    environment = "Staging"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "${random_pet.prefix.id}-aks"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix          = "${random_pet.prefix.id}-k8s"
+  kubernetes_version  = "1.29.7"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 3
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = var.appId
+    client_secret = var.password
+  }
+
+  role_based_access_control_enabled = true
+
+  tags = {
+    environment = "Staging"
+  }
+}
